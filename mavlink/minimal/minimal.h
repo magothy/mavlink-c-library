4ebb809ee653f674e5f180a6b4e9c15b1487be63
bitbucket-pipelines <commits-noreply@bitbucket.org>
2023-02-17 15:59:43 +0000

    autogenerated headers for rev 2972f5700ca00b756ce9c620f267e959eccbd3f5

--- mavlink/minimal/minimal.h
+++ mavlink/minimal/minimal.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_MINIMAL.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_MINIMAL_XML_HASH -6190440112932267329
+#define MAVLINK_MINIMAL_XML_HASH -343454138927198083
 
 #ifdef __cplusplus
 extern "C" {
